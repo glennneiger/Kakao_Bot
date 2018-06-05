ae4c26602e487bfcf9f5934b6d598f0e5fe81946
kimhyemin <>
2018-06-05 17:05:18 +0900

    no message

--- env/chatbot/views.py
+++ env/chatbot/views.py
@@ -1,9 +1,10 @@
 
 # Create your views here.
 from django.http import JsonResponse
-
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 
@@ -12,5 +13,21 @@ def keyboard(request):
     return JsonResponse({
         'type' : 'text',
     })
+
+@csrf_exempt
+def message(request):
+    message = ((request.body).decode('utf-8'))
+
+    msg = json.loads(message)
+    msg_str = msg['content']
+
+    return JsonResponse({
+        'message': {'text': "!!!\n\n" + "fkfkfkfkkfkf" + "\n\n!!!"},
+        'keyboard': {'type': 'text'}
+    })
+
+
+
+
 
 
