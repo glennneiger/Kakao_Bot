e9deac0a4a6ceaf6140f2e3804270fa476686dc0
hyemin1994 <hyemin137@gmail.com>
2018-06-05 16:56:53 +0900

    resetting

--- env/chatbot/views.py
+++ env/chatbot/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
 
 # Create your views here.
 from django.http import JsonResponse
 
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
 def keyboard(request):
 
     return JsonResponse({
-        'type' : 'buttons',
+        'type' : 'text',
     })
--- env/chatbotproject/urls.py
+++ env/chatbotproject/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-
+from chatbot import views
+from django.conf.urls import include
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^keyboard/', 'chatbot.views.keyboard'),
+	url(r'', include('chatbot.urls')),
 ]
