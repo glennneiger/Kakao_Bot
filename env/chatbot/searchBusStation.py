1891c7242fc2daa0f524893ebd400e41a99b0a80
soyoung7777 <thdud6418@gmail.com>
2018-06-21 19:56:25 +0900

    tab 수정

--- env/chatbot/searchBusStation.py
+++ env/chatbot/searchBusStation.py
@@ -4,48 +4,48 @@ import json
 import xml.etree.ElementTree as ET
 
 def serch(searchList):
-    searchST = searchList[0]
-    searchBus = ""
-    if searchST.length != 1:
-        searchBus = searchList[1]
+	searchST = searchList[0]
+	searchBus = ""
+	if searchST.length != 1:
+		searchBus = searchList[1]
 
-    text = ""
+	text = ""
 
-    ACCESS = "rxJqZMHh6oQDUSfc7Kh42uCXZuHEhmj7dY7VWber2ryr9L5t2CFRy3z834JMR7RygMzaVby7ZQ3sW%2ByCZZn0Ig%3D%3D"
+	ACCESS = "rxJqZMHh6oQDUSfc7Kh42uCXZuHEhmj7dY7VWber2ryr9L5t2CFRy3z834JMR7RygMzaVby7ZQ3sW%2ByCZZn0Ig%3D%3D"
 
-    my = "f/WM8od4VAXdGg4Q5ZaWSlJ8tIbSpw+nJ4WQ4AFRpsM"
+	my = "f/WM8od4VAXdGg4Q5ZaWSlJ8tIbSpw+nJ4WQ4AFRpsM"
 	encMy = urllib.parse.quote_plus(my)
-    encST = urllib.parse.quote_plus(searchST)
+	encST = urllib.parse.quote_plus(searchST)
 
 	odUrl = "https://api.odsay.com/v1/api/searchStation?lang=&stationName="+encST+"&apiKey="+encMy
 
-    request = urllib.request.Request(odUrl)
-    response = urllib.request.urlopen(request)
+	request = urllib.request.Request(odUrl)
+	response = urllib.request.urlopen(request)
 
-    json_rt = response.read().decode('utf-8')
-    data = json_rt.loads(json_rt)
+	json_rt = response.read().decode('utf-8')
+	data = json_rt.loads(json_rt)
 
-    stInfo = data['result']['station'][0]
+	stInfo = data['result']['station'][0]
 
-    st_name = stInfo['stationName']
-    st_ars = str(stInfo['arsID'])
+	st_name = stInfo['stationName']
+	st_ars = str(stInfo['arsID'])
 
-    st_ars = st_ars.replace("-","")
-    enArs = urllib.parse.quote_plus(st_ars)
+	st_ars = st_ars.replace("-","")
+	enArs = urllib.parse.quote_plus(st_ars)
 
-    ACCESS = "rxJqZMHh6oQDUSfc7Kh42uCXZuHEhmj7dY7VWber2ryr9L5t2CFRy3z834JMR7RygMzaVby7ZQ3sW%2ByCZZn0Ig%3D%3D"
+	ACCESS = "rxJqZMHh6oQDUSfc7Kh42uCXZuHEhmj7dY7VWber2ryr9L5t2CFRy3z834JMR7RygMzaVby7ZQ3sW%2ByCZZn0Ig%3D%3D"
 
-    oAPI = "http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid?ServiceKey="+ACCESS+"&arsId="+encArs
+	oAPI = "http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid?ServiceKey="+ACCESS+"&arsId="+encArs
 
-    tree = ET.parse(urllib.request.urlopen(oAPI))
+	tree = ET.parse(urllib.request.urlopen(oAPI))
 
-    root = tree.getroot()
-    mbody = root.find("msgBody")
+	root = tree.getroot()
+	mbody = root.find("msgBody")
 
-    busList = {}
-    cnt = 0
-    for bus in mbody.iter("itemList"):
-        text = "######"+ bus.find("arrmsg1").text+"\n"+bus.find("arrmsg1").text+"\n"+bus.find("adirection").text+"\n"+bus.find("rtNm").text+"######"
+	busList = {}
+	cnt = 0
+	for bus in mbody.iter("itemList"):
+		text = "######"+ bus.find("arrmsg1").text+"\n"+bus.find("arrmsg1").text+"\n"+bus.find("adirection").text+"\n"+bus.find("rtNm").text+"######"
         # msg1 = "msg1_c"+str(cnt)
         # msg2 = "msg2_c"+str(cnt)
         # adr = "adr_c"+str(cnt)
@@ -54,9 +54,9 @@ def serch(searchList):
         # busList[msg2] =  bus.find("arrmsg1").text
         # busList[adr] =  bus.find("adirection").text
         # busList[busNo] =  bus.find("rtNm").text
-        cnt = cnt+1
+		cnt = cnt+1
 
 
 
 
-    return text
+	return text
