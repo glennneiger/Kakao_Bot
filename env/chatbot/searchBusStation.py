5b8b77e0a58f7d5d558220e42bf55bfa000d6c2a
kimhyemin <>
2018-06-21 21:54:05 +0900

    sdfsdf

--- env/chatbot/searchBusStation.py
+++ env/chatbot/searchBusStation.py
@@ -90,6 +90,7 @@ def search(searchList):
         for i in range(0, bcnt):
             bus_key = "busNo_c"+str(i)
             if(busList[bus_key] == bus_number):
+                print(busList[bus_key])
                 kk = "msg1_c"+str(i)
                 print(kk)
 
