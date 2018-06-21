d0ecf288f83722aad71c497b3c840e0079b551be
soyoung7777 <thdud6418@gmail.com>
2018-06-21 17:54:45 +0900

    정류장 실시간 검색

---
+++ env/chatbot/searchBusStation.py
@@ -0,0 +1,56 @@
+import urllib.request
+import urllib.parse
+import json
+import xml.etree.ElementTree as ET
+
+def serch(serchST):
+    text = ""
+
+    ACCESS = "rxJqZMHh6oQDUSfc7Kh42uCXZuHEhmj7dY7VWber2ryr9L5t2CFRy3z834JMR7RygMzaVby7ZQ3sW%2ByCZZn0Ig%3D%3D"
+
+    my = "f/WM8od4VAXdGg4Q5ZaWSlJ8tIbSpw+nJ4WQ4AFRpsM"
+	encMy = urllib.parse.quote_plus(my)
+    encST = urllib.parse.quote_plus(searchST)
+
+	odUrl = "https://api.odsay.com/v1/api/searchStation?lang=&stationName="+encST+"&apiKey="+encMy
+
+    request = urllib.request.Request(odUrl)
+    response = urllib.request.urlopen(request)
+
+    json_rt = response.read().decode('utf-8')
+    data = json_rt.loads(json_rt)
+
+    stInfo = data['result']['station'][0]
+
+    st_name = stInfo['stationName']
+    st_ars = str(stInfo['arsID'])
+
+    st_ars = st_ars.replace("-","")
+    enArs = urllib.parse.quote_plus(st_ars)
+
+    ACCESS = "rxJqZMHh6oQDUSfc7Kh42uCXZuHEhmj7dY7VWber2ryr9L5t2CFRy3z834JMR7RygMzaVby7ZQ3sW%2ByCZZn0Ig%3D%3D"
+
+    oAPI = "http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid?ServiceKey="+ACCESS+"&arsId="+encArs
+
+    tree = ET.parse(urllib.request.urlopen(oAPI))
+
+    root = tree.getroot()
+    mbody = root.find("msgBody")
+
+    busList = {}
+    cnt = 0
+    for bus in mbody.iter("itemList"):
+        msg1 = "msg1_c"+str(cnt)
+        msg2 = "msg2_c"+str(cnt)
+        adr = "adr_c"+str(cnt)
+        busNo = "busNo_c"+str(cnt)
+        busList[msg1] =  bus.find("arrmsg1").text
+        busList[msg2] =  bus.find("arrmsg1").text
+        busList[adr] =  bus.find("adirection").text
+        busList[busNo] =  bus.find("rtNm").text
+        cnt = cnt+1
+
+    ###버스 정보
+
+
+    return text
