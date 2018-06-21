397eca2c5a0b273b42781294a4e8053d46f47394
soyoung7777 <thdud6418@gmail.com>
2018-06-21 20:01:47 +0900

    test

--- env/chatbot/views.py
+++ env/chatbot/views.py
@@ -198,6 +198,7 @@ def incomFalse(intent_name, data):
     elif intent_name == "Bus_Info":
         print("AAAAAAAAA")
         searchList = data['result']['parameters']['bus_info']
+        print(searchList)
         text = searchBusStation(searchList)
     elif intent_name == "Default Fallback Intent":
         text = str(data['result']['fulfillment']['messages'][0]['speech'])
