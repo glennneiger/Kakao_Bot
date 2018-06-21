cd4121a7dd7a6cbf3c2721ebf2444454eb382c9e
kimhyemin <>
2018-06-22 08:11:32 +0900

    sas

--- env/chatbot/searchBusStation.py
+++ env/chatbot/searchBusStation.py
@@ -105,10 +105,13 @@ def search(searchList):
 
 
         print(arrival_first)
+
         if eq(arrival_first,"곧 도착") != True:
             for i in range(0,len(arrival_first)):
                 if eq(arrival_first[i],"["):
                     arrival_busstation.append(arrival_first[i+1])
+        else :
+            arrival_busstation.append("0")
 
         print(arrival_second)
         if eq(arrival_second,"곧 도착") != True:
