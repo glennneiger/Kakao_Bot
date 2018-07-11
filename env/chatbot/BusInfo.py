3fce6d4b3846269ab700adc0db76481d71683a9d
kimhyemin <>
2018-07-11 14:27:02 +0900

    no message

--- env/chatbot/BusInfo.py
+++ env/chatbot/BusInfo.py
@@ -68,7 +68,7 @@ def get_bus_direction(stationName):
     encMy = urllib.parse.quote_plus(my)
 
     for i in range(0,len(bus_station_id[stationName])):
-        odUrl = "https://api.odsay.com/v1/api/searchStation?lang=&stationName="+str(bus_station_id[stationName][i])+"&CID=1000&stationClass=1&apiKey="+encMy
+        odUrl = "https://api.odsay.com/v1/api/busStationInfo?lang=0&stationID="+str(bus_station_id[stationName][i])+"&apiKey="+encMy
         request = urllib.request.Request(odUrl)
         response = urllib.request.urlopen(request)
 
