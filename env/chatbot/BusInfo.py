e0c5a2b811a6a6aee3e5d78e720199628c1a5434
kimhyemin <>
2018-08-25 14:57:56 +0900

    no message

--- env/chatbot/BusInfo.py
+++ env/chatbot/BusInfo.py
@@ -81,9 +81,8 @@ def get_bus_station_information(busData):
     encMy = urllib.parse.quote_plus(my)
 
     for i in range(0,len(bus_arsid[bus_station])) :
-        encArs = urllib.parse.quote_plus(bus_arsid[bus_station][i])
-        print("encArs\n")
-        oAPI = "http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid?ServiceKey="+ACCESS+"&arsId="+encArs
+        #encArs = urllib.parse.quote_plus(bus_arsid[bus_station][i])
+        oAPI = "http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid?ServiceKey="+ACCESS+"&arsId="+bus_arsid[bus_station][i]
         tree = ET.parse(urllib.request.urlopen(oAPI))
 
         root = tree.getroot()
